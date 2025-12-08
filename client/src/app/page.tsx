d1f9bfa4c1dcd96a38815daccaf086741485948b
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2025-12-08 06:51:51 +0300

    created a login and signup page folders

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,17 +1,6 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Image
-          src="/logo.png"
-          alt="JobForge Logo"
-          width={300}
-          height={100}
-          priority
-        />
-      </div>
-    </main>
-  );
+  redirect("/login");
+  return null;
 }
