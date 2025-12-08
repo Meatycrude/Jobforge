f97316447f088e72897a5d3e145c75fe166b6db3
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2025-12-08 14:48:35 +0300

    dashboard properties

---
+++ client/src/app/(user)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export default function Dashboard() {
+  return (
+    <>
+      <div className=" container mx-auto h-screen w-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 mx-2 gap-4">
+        <div className="bg-black "></div>
+        <div className="bg-amber-600 "></div>
+        <div className="bg-slate-300  "></div>
+      </div>
+    </>
+  );
+}
